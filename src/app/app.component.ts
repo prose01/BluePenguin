5bb467435fbcacf86b7aa8d7455ac6d06260ab31
Peter Rose <peterrose03@gmail.com>
2017-06-30 08:00:31 +0200

    1 The Profile Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Profile {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+	  <h1>{{title}}</h1>
+	  <h2>{{profile.name}} details!</h2>
+	  <div><label>id: </label>{{profile.id}}</div>
+	  <div>
+	      <label>name: </label>
+	      <input [(ngModel)]="profile.name" placeholder="name">
+	  </div>
+  `
 })
+
 export class AppComponent {
   title = 'BluePenguins';
+  profile : Profile = {
+	  id: 1,
+	  name: 'Windstorm'
+	};
 }
