99c13507d5937ca43a15f86d746cb4e877f10570
Peter Rose <peterrose03@gmail.com>
2018-06-26 20:16:52 +0200

    Forgot to commit edit-profiles.component

---
+++ src/app/edit-profile/edit-profile.component.ts
@@ -0,0 +1,79 @@
+import 'rxjs/add/operator/switchMap';
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { Profile } from '../models/profile';
+import { ProfileService } from '../services/profile.service';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
+
+export class EditProfileComponent implements OnChanges {
+	@Input() profile : Profile;
+	profileForm: FormGroup;
+
+  	constructor(
+	  private profileService: ProfileService,
+	  private route: ActivatedRoute,
+	  private location: Location,
+	  private fb: FormBuilder) { this.createForm(); }
+
+  	createForm() {
+	    this.profileForm = this.fb.group({
+	      name: ['', Validators.required ],
+	      body: ''
+	    });
+  	}
+
+  	ngOnChanges() { 
+    	this.rebuildForm();
+  	}
+
+  	rebuildForm() {
+  		this.profileForm.reset({
+	      name: this.profile.name
+	    });
+    	// this.profileForm.reset();
+    	// this.profileForm.patchValue({
+     //  		name: this.profile.name
+    	// });
+  	}
+
+  	onSubmit() {
+	  this.profile = this.prepareSaveProfile();
+	  this.profileService.addProfile(this.profile).subscribe(/* add error handling */);
+	  this.rebuildForm();
+	}
+
+	prepareSaveProfile(): Profile {
+    const formModel = this.profileForm.value;
+
+    const saveProfile: Profile = {
+	      profileId: this.profile.profileId,
+	      name: formModel.name as string,
+	      body: formModel.body as string,
+	      updatedOn: formModel.name as string,	// sæt til ingenting eller datetime.now
+	      createdOn: formModel.name as string
+	    };
+	    return saveProfile;
+	}
+
+  	revert() { this.rebuildForm(); }
+
+  	ngOnInit(): void {
+	  this.route.paramMap
+	    .switchMap((params: ParamMap) => this.profileService.getProfile(params.get('profileId')))
+	    .subscribe(profile => this.profile = profile);
+	    //this.rebuildForm();
+	}
+
+	goBack(): void {
+	  this.location.back();
+	}
+
+}
