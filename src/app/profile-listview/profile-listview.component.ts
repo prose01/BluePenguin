92f3d8ba5af411b78197e8523a7a07463bcc9832
lripro <peter.rose@lrforlag.dk>
2019-10-07 16:43:08 +0200

    Added expandable rows to Profile-Listview-Component.

--- src/app/profile-listview/profile-listview.component.ts
+++ src/app/profile-listview/profile-listview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { Profile } from '../models/profile';
 import { ProfileService } from '../services/profile.service';
@@ -8,12 +9,20 @@ import { ProfileService } from '../services/profile.service';
 @Component({
     selector: 'app-profile-listview',
     templateUrl: './profile-listview.component.html',
-    styleUrls: ['./profile-listview.component.css']
+    styleUrls: ['./profile-listview.component.css'],
+    animations: [
+        trigger('detailExpand', [
+            state('collapsed', style({ height: '0px', minHeight: '0' })),
+            state('expanded', style({ height: '*' })),
+            transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+        ]),
+    ],
 })
 export class ProfileListviewComponent implements OnInit {
     profiles: Profile[];
-    displayedColumns: string[] = ['profileId', 'name', 'email', 'createdOn'];
+    displayedColumns: string[] = ['profileId', 'name', 'email'];
     dataSource: MatTableDataSource<Profile>;
+    expandedElement: Profile | null;
 
     @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
     @ViewChild(MatSort, { static: false }) sort: MatSort;
