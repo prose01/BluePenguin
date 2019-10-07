d6ca2fdb340ca75d89f2b58d4865c038ccbe48d1
lripro <peter.rose@lrforlag.dk>
2019-10-07 15:37:04 +0200

    Added {static: false} to MatPaginator & MatSort ViewChild.

--- src/app/profile-listview/profile-listview.component.ts
+++ src/app/profile-listview/profile-listview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef  } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
@@ -6,35 +6,35 @@ import { Profile } from '../models/profile';
 import { ProfileService } from '../services/profile.service';
 
 @Component({
-  selector: 'app-profile-listview',
-  templateUrl: './profile-listview.component.html',
-  styleUrls: ['./profile-listview.component.css']
+    selector: 'app-profile-listview',
+    templateUrl: './profile-listview.component.html',
+    styleUrls: ['./profile-listview.component.css']
 })
-export class ProfileListviewComponent implements OnInit  {
-  profiles: Profile[];
-  displayedColumns: string[] = ['profileId', 'name', 'email', 'createdOn'];
-  dataSource: MatTableDataSource<Profile>;
+export class ProfileListviewComponent implements OnInit {
+    profiles: Profile[];
+    displayedColumns: string[] = ['profileId', 'name', 'email', 'createdOn'];
+    dataSource: MatTableDataSource<Profile>;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+    @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+    @ViewChild(MatSort, { static: false }) sort: MatSort;
 
-  constructor(private router: Router, private profileService: ProfileService, private cdr: ChangeDetectorRef) { }  
+    constructor(private router: Router, private profileService: ProfileService, private cdr: ChangeDetectorRef) { }
 
-  ngOnInit(): void {
-    this.getProfiles();
-  }
+    ngOnInit(): void {
+        this.getProfiles();
+    }
 
-  getProfiles(): void {
-    this.profileService.getProfiles().subscribe((result) => {
-      this.dataSource = new MatTableDataSource<Profile>(result);
+    getProfiles(): void {
+        this.profileService.getProfiles().subscribe((result) => {
+            this.dataSource = new MatTableDataSource<Profile>(result);
 
-      this.cdr.detectChanges(); // Needed to get pagination & sort working.
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    })
-  }
+            this.cdr.detectChanges(); // Needed to get pagination & sort working.
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+        })
+    }
 
-  onSelect(profile: Profile): void {
-    this.router.navigate(['/detail', profile.profileId]);
-  }
+    onSelect(profile: Profile): void {
+        this.router.navigate(['/detail', profile.profileId]);
+    }
 }
