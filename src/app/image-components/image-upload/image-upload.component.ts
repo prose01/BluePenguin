bd3349944033281bc3983f6ea7f67c69171fa94c
Peter <peterrose03@gmail.com>
2021-01-18 19:57:04 +0100

    Downgraded TypeScript

--- src/app/image-components/image-upload/image-upload.component.ts
+++ src/app/image-components/image-upload/image-upload.component.ts
@@ -10,7 +10,6 @@ import { Router } from '@angular/router';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { ImageCroppedEvent, ImageTransform } from '../image-cropper/interfaces/index';
 import { base64ToFile } from '../image-cropper/utils/blob.utils';
-import { AutoUnsubscribe, takeWhileAlive } from 'take-while-alive';
 
 import { AuthService } from '../../authorisation/auth/auth.service';
 import { ImageService } from '../../services/image.service';
@@ -21,7 +20,6 @@ import { ImageService } from '../../services/image.service';
   styleUrls: ['./image-upload.component.scss']
 })
 
-@AutoUnsubscribe()
 export class ImageUploadComponent {
   uploadImageForm: FormGroup;
   imageChangedEvent: any = '';
@@ -164,7 +162,7 @@ export class ImageUploadComponent {
       formData.append('image', base64ToFile(this.croppedImage));
       formData.append('title', uploadModel.title as string);
 
-      this.imageService.uploadImage(formData).pipe(takeWhileAlive(this)).subscribe(() => { }, () => { this.router.navigate(['/imagesboard']); }, () => { this.router.navigate(['/imagesboard']); });
+      this.imageService.uploadImage(formData).subscribe(() => { }, () => { this.router.navigate(['/imagesboard']); }, () => { this.router.navigate(['/imagesboard']); });
     }
 
     //setTimeout(() => { this.router.navigate(['/imagesboard']); }, 500);
