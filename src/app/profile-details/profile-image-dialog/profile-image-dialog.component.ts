ef7a76a88eeb88998154e207bce4593d6fb32b77
Peter <peterrose03@gmail.com>
2021-01-25 16:40:53 +0100

    Added nav button to image gallery

--- src/app/profile-details/profile-image-dialog/profile-image-dialog.component.ts
+++ src/app/profile-details/profile-image-dialog/profile-image-dialog.component.ts
@@ -3,13 +3,33 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'profile-image-dialog',
-  templateUrl: './profile-image-dialog.component.html'
+  templateUrl: './profile-image-dialog.component.html',
+  styleUrls: ['./profile-image-dialog.component.scss']
 })
 
 export class ProfileImageDialog {
+  index: number;
 
   constructor(public dialogRef: MatDialogRef<ProfileImageDialog>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.index = this.data.index;
+  }
+
+  onCloseClick(): void {
+    this.dialogRef.close();
+  }
+
+  onNextClick(): void {
+    if ((this.data.images.length - 1) > this.index) {
+      this.index++;
+    }
+  }
+
+  onBeforeClick(): void {
+    if (0 < (this.data.images.length - 1) && 0 < this.index) {
+      this.index--;
+    }
   }
 
 }
