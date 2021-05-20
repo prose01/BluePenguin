a7beba880b9746a839769920a1801601192b4309
Peter <peterrose03@gmail.com>
2021-05-20 14:54:47 +0200

    Added handleError

--- src/app/chat/signalr-adapter.ts
+++ src/app/chat/signalr-adapter.ts
@@ -1,7 +1,7 @@
 import { ChatAdapter, Message, ParticipantResponse, IChatParticipant } from 'ng-chat';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { map, catchError, delay } from 'rxjs/operators';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 import * as signalR from "@aspnet/signalr";
 import { AuthService } from '../authorisation/auth/auth.service';
@@ -85,8 +85,27 @@ export class SignalRAdapter extends ChatAdapter {
   //TODO: Helper Lav en rigtig error handler inden produktion
   // https://stackblitz.com/angular/jyrxkavlvap?file=src%2Fapp%2Fheroes%2Fheroes.service.ts
   // Husk at opdater GET, POST etc this.handleError!
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+  //private handleError(error: any): Promise<any> {
+  //  console.error('An error occurred', error); // for demo purposes only
+  //  return Promise.reject(error.message || error);
+  //}
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('No connection to chat server:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
