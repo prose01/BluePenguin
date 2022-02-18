cbbb9a086463ad3e7a2f2c632d0f47bdff5e34cb
Peter <peterrose03@gmail.com>
2022-02-18 01:07:11 +0100

    ng update rxjs

--- src/app/authorisation/auth/auth.interceptor.ts
+++ src/app/authorisation/auth/auth.interceptor.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/fromPromise';
-import 'rxjs/add/operator/mergeMap';
+import { Observable, from } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -12,28 +11,56 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   private isAuthenticated() : Observable<boolean> {
-    return Observable.fromPromise(Promise.resolve(this.authService.isAuthenticated()));
+    return from(Promise.resolve(this.authService.isAuthenticated()));
   }
 
   private getAccessToken() : Observable<string> {
-    return Observable.fromPromise(Promise.resolve(this.authService.getAccessToken()));
+    return from(Promise.resolve(this.authService.getAccessToken()));
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return this.isAuthenticated().mergeMap((isAuthenticated) => {
+    return this.isAuthenticated().pipe(mergeMap((isAuthenticated) => {
       if (!isAuthenticated) {
         return next.handle(request);
       }
 
-      return this.getAccessToken().mergeMap((accessToken) => {
+      return this.getAccessToken().pipe(mergeMap((accessToken) => {
           request = request.clone({
             setHeaders: {
               Authorization: `Bearer ${accessToken}`
             }
           });
           return next.handle(request);
-        })
-    });
+        }))
+
+        //.mergeMap((accessToken) => {
+        //  request = request.clone({
+        //    setHeaders: {
+        //      Authorization: `Bearer ${accessToken}`
+        //    }
+        //  });
+        //  return next.handle(request);
+        //})
+
+
+    }
+
+    ));
+
+      //.mergeMap((isAuthenticated) => {
+      //if (!isAuthenticated) {
+      //  return next.handle(request);
+      //}
+
+      //return this.getAccessToken().mergeMap((accessToken) => {
+      //    request = request.clone({
+      //      setHeaders: {
+      //        Authorization: `Bearer ${accessToken}`
+      //      }
+      //    });
+      //    return next.handle(request);
+      //  })
+    //});
   }
 
   
